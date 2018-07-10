9cff979e80a795a5e4dd74c1e442b8dc0cc9e9ed
Pepe Valdivia <jvaldivia@softweb.pe>
2018-07-09 20:27:58 -0500

    configs/constants agregado

---
+++ configs/constants.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+M.BASE_URL = 'http://localhost:9090/'
+M.STATIC_URL = 'http://localhost:9090/static/'
+M.SISTEMA_ID = '4'
+M.CSRF = {
+  key = 'csrf-key',
+  secret = 'valueUnknow',
+}
+M.SERVICIOS = {
+  accesos = {
+    url = 'http://localhost:5000/',
+    csrf_key = 'csrf_val',
+    csrf_value = 'PKBcauXg6sTXz7Ddlty0nejVgoUodXL89KNxcrfwkEme0Huqtj6jjt4fP7v2uF4L',
+  },
+}
+M.AMBIENTE_STATIC = 'desarrollo'
+
+return M
