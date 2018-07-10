3b73ea1f4c17160bfb49a8d3de757e191089a287
Pepe Valdivia <jvaldivia@softweb.pe>
2018-07-09 20:26:28 -0500

    Scaffold de la aplicación

--- app.lua
+++ app.lua
@@ -1,8 +1,14 @@
 local lapis = require('lapis')
+local respond_to = require('lapis.application').respond_to
 local app = lapis.Application()
+app:enable('etlua')
 
 app:get('/', function()
   return 'Welcome to Lapis ' .. require('lapis.version')
+end)
+
+app:match("/test/conexion", function(self)
+  return "ok"
 end)
 
 return app
